80808289bb59b5c12de8e228ed1afbb0a9658e46
John Ombagi <ombagi@outlook.com>
2017-05-29 12:08:32 +0300

    Script to get jdk-8u45 x64 Linux

---
+++ Postinstalls/get-jdk-8u45-linux-x64.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -c -O "jdk-8u45-linux-x64.tar.gz" --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
