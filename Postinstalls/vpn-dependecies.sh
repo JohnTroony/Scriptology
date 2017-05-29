1ca5aa66cac9f5173e76dc4bca47b6d090d2167f
John Ombagi <ombagi@outlook.com>
2017-05-29 12:06:25 +0300

    Add VPN client dependecies on Kali Linux

---
+++ Postinstalls/vpn-dependecies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install g++
+apt-get install build-essential
+apt-get install flex
+apt-get install libedit2 libedit-dev
+apt-get install bison
+apt-get install cmake
+apt-get install openssl
+apt-get install libqt4-dev
+
